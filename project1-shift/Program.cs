1895ad2ae9c05e651349660fc9ab535859a1f1d5
Sezerk15 <143318972+Sezerk15@users.noreply.github.com>
2023-10-30 11:06:15 +0300

    byte commit

--- project1-shift/Program.cs
+++ project1-shift/Program.cs
@@ -2,27 +2,28 @@
 
 
 
+byte roundCount = 0;
 int turnCounter = 0;
 string turnName = "Initiator";
 string firstRow = "     ";
 string secondRow = "     ";
 string thirdRow = "     ";
-short boardScore = 0;
-short AA;
-short AB;
-short AC;
-short BA;
-short BB;
-short BC;
-short CA;
-short CB;
-short CC;
-short playerScore = 0;
+byte boardScore = 0;
+byte AA = 0;
+byte AB = 0;
+byte AC = 0;
+byte BA = 0;
+byte BB = 0;
+byte BC = 0;
+byte CA = 0;
+byte CB = 0;
+byte CC = 0;
+byte playerScore = 0;
 int computerScore = 0;
 string unusedDigits = "1 2 3 4 5 6 7 8 9";
 bool gameStart = false;
 
-Console.WriteLine("------------------ Round 0 ------------------\n");
+Console.WriteLine($"------------------ Round {roundCount} ------------------\n");
 
 Console.WriteLine($"    1 2 3          Turn : {turnCounter} / {turnName}               Digits left:");
 Console.WriteLine($"  + - - - +                                         {unusedDigits}");
@@ -33,99 +34,106 @@ Console.WriteLine($"  + - - - +        Computer Score : {computerScore}         
 Console.WriteLine("                                                  ");
 Console.WriteLine("Selected digit :                                   ");
 
-
-string userInput;
-short inputCounter = 0;
+byte inputCounter = 0;
 string tempString;
 bool included;
-short userInputShort;
+byte userInputByte;
 do
 {
     Console.SetCursorPosition(17, 9);
-    userInput = Console.ReadLine();
-    userInputShort = Convert.ToInt16(userInput);
-    switch (userInput)
+
+    userInputByte = Convert.ToByte(Console.ReadLine());
+    switch (userInputByte)
     {
-        case "1":
-        case "2":
-        case "3":
-        case "4":
-        case "5":
-        case "6":
-        case "7":
-        case "8":
-        case "9":
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
             included = false;
-            if (AA == userInputShort) included = true;
-            if (AB == userInputShort) included = true;
-            if (AC == userInputShort) included = true;
-            if (BA == userInputShort) included = true;
-            if (BB == userInputShort) included = true;
-            if (BC == userInputShort) included = true;
-            if (CA == userInputShort) included = true;
-            if (CB == userInputShort) included = true;
-            if (CC == userInputShort) included = true;
+            if (AA == userInputByte) included = true;
+            if (AB == userInputByte) included = true;
+            if (AC == userInputByte) included = true;
+            if (BA == userInputByte) included = true;
+            if (BB == userInputByte) included = true;
+            if (BC == userInputByte) included = true;
+            if (CA == userInputByte) included = true;
+            if (CB == userInputByte) included = true;
+            if (CC == userInputByte) included = true;
             if (!included)
             {
                 inputCounter++;
                 tempString = "";
-                for (int i = 0; i < 2 * Convert.ToInt16(userInput) - 2;i++)
+                for (int i = 0; i < 2 * userInputByte - 2;i++)
                     tempString += unusedDigits[i];
                 tempString += 'X';
-                for (int i = 2 * Convert.ToInt16(userInput) - 1; i < unusedDigits.Length; i++)
+                for (int i = 2 * userInputByte - 1; i < unusedDigits.Length; i++)
                     tempString += unusedDigits[i];
                 unusedDigits = tempString;
-                
+                switch (inputCounter)
+                {
+                    case 1:
+                        AA = userInputByte;
+                        firstRow = AA + "    ";
+                        break;
+                    case 2:
+                        AB = userInputByte;
+                        firstRow = AA + " " + AB + "  ";
+                        break;
+                    case 3:
+                        AC = userInputByte;
+                        firstRow = AA + " " + AB + " " + AC;
+                        break;
+                    case 4:
+                        BA = userInputByte;
+                        secondRow = BA + "    ";
+                        break;
+                    case 5:
+                        BB = userInputByte;
+                        secondRow = BA + " " + BB + "  ";
+                        break;
+                    case 6:
+                        BC = userInputByte;
+                        secondRow = BA + " " + BB + " " + BC;
+                        break;
+                    case 7:
+                        CA = userInputByte;
+                        thirdRow = CA + "    ";
+                        break;
+                    case 8:
+                        CB = userInputByte;
+                        thirdRow = CA + " " + CB + "  ";
+                        break;
+                    case 9:
+                        CC = userInputByte;
+                        thirdRow = CA + " " + CB + " " + CC;
+                        gameStart = true;
+                        break;
+                    default:
+                        break;
+                }
+
             }
-
-
             break;
         default: 
             break;
 
     }
-    switch (inputCounter)
-    {
-        case 1:
-            AA = userInputShort;
-            break;
-        case 2:
-            AB = userInputShort;
-            break;
-        case 3:
-            AC = userInputShort;
-            break;
-        case 4:
-            BA = userInputShort;
-            break;
-        case 5:
-            BB = userInputShort;
-            break;
-        case 6:
-            BC = userInputShort;
-            break;
-        case 7:
-            CA = userInputShort;
-            break;
-        case 8:
-            CB = userInputShort;
-            break;
-        case 9:
-            CC = userInputShort;
-            gameStart = true;
-            break;
-        case 0:
-        default:
-            break;
-    }
+
+
+
     Console.Clear();
-    Console.WriteLine("------------------ Round 0 ------------------\n");
+    Console.WriteLine($"------------------ Round {roundCount} ------------------\n");
 
     Console.WriteLine($"    1 2 3          Turn : {turnCounter} / {turnName}               Digits left:");
     Console.WriteLine($"  + - - - +                                         {unusedDigits}");
-    Console.WriteLine($"1 | {AA} {AB} {AC} |        Board Score    : {boardScore}        ");
-    Console.WriteLine($"2 | {BA} {BB} {BC} |                                 ");
-    Console.WriteLine($"3 | {CA} {CB} {CC} |        Player Score   : {playerScore}        ");
+    Console.WriteLine($"1 | {firstRow} |        Board Score    : {boardScore}        ");
+    Console.WriteLine($"2 | {secondRow} |                                 ");
+    Console.WriteLine($"3 | {thirdRow} |        Player Score   : {playerScore}        ");
     Console.WriteLine($"  + - - - +        Computer Score : {computerScore}         ");
     Console.WriteLine("                                                  ");
     Console.WriteLine("Selected digit :                                   ");
@@ -133,14 +141,20 @@ do
 
 } while (gameStart == false);
 
-Console.Clear();
-Console.WriteLine("------------------ Round 0 ------------------\n");
+do
+{
+    turnName = "Player";
+    turnCounter = 1;
+    roundCount++;
+    Console.Clear();
+    Console.WriteLine($"------------------ Round {roundCount} ------------------\n");
 
-Console.WriteLine($"    1 2 3          Turn : {turnCounter} / {turnName}               Digits left:");
-Console.WriteLine($"  + - - - +                                         {unusedDigits}");
-Console.WriteLine($"1 | {AA} {AB} {AC} |        Board Score    : {boardScore}        ");
-Console.WriteLine($"2 | {BA} {BB} {BC} |                                 ");
-Console.WriteLine($"3 | {CA} {CB} {CC} |        Player Score   : {playerScore}        ");
-Console.WriteLine($"  + - - - +        Computer Score : {computerScore}         ");
-Console.WriteLine("                                                  ");
-Console.WriteLine("Selected digit :                                   ");
+    Console.WriteLine($"    1 2 3          Turn : {turnCounter} / {turnName}");
+    Console.WriteLine($"  + - - - +                                        ");
+    Console.WriteLine($"1 | {firstRow} |        Board Score    : {boardScore}        ");
+    Console.WriteLine($"2 | {secondRow} |                                 ");
+    Console.WriteLine($"3 | {thirdRow} |        Player Score   : {playerScore}        ");
+    Console.WriteLine($"  + - - - +        Computer Score : {computerScore}         ");
+    Console.WriteLine("                                                  ");
+    Console.WriteLine("Selected digit :                                   ");
+} while (roundCount < 5);
