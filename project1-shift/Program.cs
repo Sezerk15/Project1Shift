134945405038bcca6a2d0946827691ebf1cb2962
Berdowski22 <130123856+Berdowski22@users.noreply.github.com>
2023-11-03 10:54:46 +0300

    son commit

--- project1-shift/Program.cs
+++ project1-shift/Program.cs
@@ -1,42 +1,27 @@
 ﻿
-
-
-                                          // Initialization of necessary variables.
-byte roundCount = 0;
+int roundCount = 0;
 int turnCounter = 0;
 string turnName = "Initiator";
-
-                                    // Rows of the board are stored as strings.
 string firstRow = "     ";
 string secondRow = "     ";
 string thirdRow = "     ";
-
-                             // Board and player scores are stored as bytes since they can only be 0, 1, 4 or 9.
-byte boardScore = 0;
-byte playerScore = 0;
+int boardScore = 0;
+int AA = 0;
+int AB = 0;
+int AC = 0; 
+int BA = 0;
+int BB = 0;
+int BC = 0;
+int CA = 0;
+int CB = 0;
+int CC = 0;
+int playerScore = 0;
 int computerScore = 0;
-
-                    // Digit 'slots' are stored as individual variables. AA represents i1j1 AB represents i1j2 etc.
-byte AA = 0;
-byte AB = 0;
-byte AC = 0;
-byte BA = 0;
-byte BB = 0;
-byte BC = 0;
-byte CA = 0;
-byte CB = 0;
-byte CC = 0;
-
-
 string unusedDigits = "1 2 3 4 5 6 7 8 9";
 bool gameStart = false;
-byte inputCounter = 0;
-string tempString;
-bool included;
-byte userInputByte;
 
-                                 // Below is a blueprint of the game board.
 Console.WriteLine($"------------------ Round {roundCount} ------------------\n");
+
 Console.WriteLine($"    1 2 3          Turn : {turnCounter} / {turnName}               Digits left:");
 Console.WriteLine($"  + - - - +                                         {unusedDigits}");
 Console.WriteLine($"1 |       |        Board Score    : {boardScore}        ");
@@ -46,12 +31,15 @@ Console.WriteLine($"  + - - - +        Computer Score : {computerScore}         
 Console.WriteLine("                                                  ");
 Console.WriteLine("Selected digit :                                   ");
 
-                                      // GAME BOARD INITIALIZATION LOOP
+int inputCounter = 0;
+string tempString;
+bool included;
+int userInputByte;
 do
 {
     Console.SetCursorPosition(17, 9);
-                                 // Input is stored as byte since only digits from 1 to 9 are accepted.
-    userInputByte = Convert.ToByte(Console.ReadLine());
+
+    userInputByte = Convert.ToInt16(Console.ReadLine());
     switch (userInputByte)
     {
         case 1:
@@ -74,20 +62,20 @@ do
             if (CB == userInputByte) included = true;
             if (CC == userInputByte) included = true;
             if (!included)
-            {                    // Code below only works if given digit has not yet been included in the board.
+            {
                 inputCounter++;
                 tempString = "";
-                for (byte i = 0; i < 2 * userInputByte - 2;i++)
+                for (int i = 0; i < 2 * userInputByte - 2;i++)
                     tempString += unusedDigits[i];
                 tempString += 'X';
                 for (int i = 2 * userInputByte - 1; i < unusedDigits.Length; i++)
                     tempString += unusedDigits[i];
                 unusedDigits = tempString;
                 switch (inputCounter)
-                {                         // Board is updated top-to-bottom left-to-right with every 'correct' input.
+                {
                     case 1:
                         AA = userInputByte;
-                        firstRow = AA + "    "; 
+                        firstRow = AA + "    ";
                         break;
                     case 2:
                         AB = userInputByte;
@@ -117,7 +105,7 @@ do
                         CB = userInputByte;
                         thirdRow = CA + " " + CB + "  ";
                         break;
-                    case 9:           // Once 9 digits have been entered, the initialization stage ends and the game begins.
+                    case 9:
                         CC = userInputByte;
                         thirdRow = CA + " " + CB + " " + CC;
                         gameStart = true;
@@ -134,7 +122,7 @@ do
     }
 
 
-                                       // Board is updated with every input.
+
     Console.Clear();
     Console.WriteLine($"------------------ Round {roundCount} ------------------\n");
 
@@ -152,10 +140,122 @@ do
 
 do
 {
+    bool turn = false; //to define whose turn is this//
     turnName = "Player";
     turnCounter = 1;
     roundCount++;
+    int point = 0;
     Console.Clear();
+    if ((AA == AB + 1 && AB == AC + 1) || (AC == AB + 1 && AB == AA + 1))//checks first row//
+    {
+        point++;
+    }
+    if ((BA == BB + 1 && BB == BC + 1) || (BC == BB + 1 && BB == BA + 1))//checks second row(two direction)//
+    {
+        point++;
+    }
+    if ((CA == CB + 1 && CB == CC + 1) || (CC == CB + 1 && CB == CA + 1))//checks third row(two direction)//
+    {
+        point++;
+    }
+    if ((AA == BA + 1 && BA == CA + 1) || (BA == AA + 1 && CA == BA + 1))//checks first column(two direction)//
+    {
+        point++;
+    }
+    if ((AB == BB + 1 && BB == CB + 1) || (BB == AB + 1 && CB == BB + 1))//checks second column(two direction)//
+    {
+        point++;
+    }
+    if ((AC == BC + 1 && BC == CC + 1) || (BC == AC + 1 && CC == BC + 1))//checks third column(two direction)//
+    {
+        point++;
+    }
+    if ((AA== BB + 1 && BB == CC + 1) || (BB == AA + 1 && CC == BB + 1))//checks diagonal from left top to right bottom //
+    {
+        point++;
+    }
+    if ((AC == BB + 1 && BB == CA + 1) || (CA == BB + 1 && BB== AC + 1))//checks diagonal from right top to left bottom//
+    {
+        point++;
+    }
+    boardScore = point * point;
+
+    if (boardScore == 0 && turn == false) //if turn is false then it is player's point//
+    {
+        if ((AA == AB + 1 && AB == AC + 1) || (AC == AB + 1 && AB == AA + 1))
+        {
+            point++;
+        }
+        if ((BA == BB + 1 && BB == BC + 1) || (BC == BB + 1 && BB == BA + 1))
+        {
+            point++;
+        }
+        if ((CA == CB + 1 && CB == CC + 1) || (CC == CB + 1 && CB == CA + 1))
+        {
+            point++;
+        }
+        if ((AA == BA + 1 && BA == CA + 1) || (BA == AA + 1 && CA == BA + 1))
+        {
+            point++;
+        }
+        if ((AB == BB + 1 && BB == CB + 1) || (BB == AB + 1 && CB == BB + 1))
+        {
+            point++;
+        }
+        if ((AC == BC + 1 && BC == CC + 1) || (BC == AC + 1 && CC == BC + 1))
+        {
+            point++;
+        }
+        if ((AA == BB + 1 && BB == CC + 1) || (BB == AA + 1 && CC == BB + 1))
+        {
+            point++;
+        }
+        if ((AC == BB + 1 && BB == CA + 1) || (CA == BB + 1 && BB == AC + 1))
+        {
+            point++;
+        }
+        boardScore = point * point;
+        playerScore = boardScore;
+    }
+    else if (boardScore == 0 && turn == true) //if not the point is computer's//
+    {
+        if ((AA == AB + 1 && AB == AC + 1) || (AC == AB + 1 && AB == AA + 1))
+        {
+            point++;
+        }
+        if ((BA == BB + 1 && BB == BC + 1) || (BC == BB + 1 && BB == BA + 1))
+        {
+            point++;
+        }
+        if ((CA == CB + 1 && CB == CC + 1) || (CC == CB + 1 && CB == CA + 1))
+        {
+            point++;
+        }
+        if ((AA == BA + 1 && BA == CA + 1) || (BA == AA + 1 && CA == BA + 1))
+        {
+            point++;
+        }
+        if ((AB == BB + 1 && BB == CB + 1) || (BB == AB + 1 && CB == BB + 1))
+        {
+            point++;
+        }
+        if ((AC == BC + 1 && BC == CC + 1) || (BC == AC + 1 && CC == BC + 1))
+        {
+            point++;
+        }
+        if ((AA == BB + 1 && BB == CC + 1) || (BB == AA + 1 && CC == BB + 1))
+        {
+            point++;
+        }
+        if ((AC == BB + 1 && BB == CA + 1) || (CA == BB + 1 && BB == AC + 1))
+        {
+            point++;
+        }
+        boardScore = point * point;
+        computerScore = boardScore;
+    }
+
+
     Console.WriteLine($"------------------ Round {roundCount} ------------------\n");
 
     Console.WriteLine($"    1 2 3          Turn : {turnCounter} / {turnName}");
@@ -166,4 +266,6 @@ do
     Console.WriteLine($"  + - - - +        Computer Score : {computerScore}         ");
     Console.WriteLine("                                                  ");
     Console.WriteLine("Selected digit :                                   ");
+
+    turn = true;
 } while (roundCount < 5);
